64ccfb31b27b3eff79fb0f79d05cdcf23200411d
kaush <emailtokaush@gmail.com>
2019-11-02 12:49:47 -0400

    hand_score.rb

---
+++ basic-problems/hand_score.rb
@@ -0,0 +1,13 @@
+# Hand Score
+# Write a method hand_score that takes in a string representing a hand of cards and returns it's total score. 
+# You can assume the letters in the string are only A, K, Q, J. A is worth 4 points, K is 3 points, Q is 2 points, 
+# and J is 1 point. The letters of the input string not necessarily uppercase.
+
+def hand_score(str)
+hash = {"A" => 4, "K" => 3,"Q" => 2, "J" => 1}
+count = 0
+str.each_char{|char| count += hash[char.upcase] if hash.has_key?(char.upcase)}
+count
+end
+puts hand_score("AQAJ") #=> 11
+puts hand_score("jJka") #=> 9
